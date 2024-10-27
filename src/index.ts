50dfd7e07032bf64160089116ef535efd377e46a
amit-singhs <amitdrive003@gmail.com>
2024-10-27 18:04:28 +0530

    - Undoing the previous change, since it didn't fix the issue, but instead the form values are not saving.

--- src/index.ts
+++ src/index.ts
@@ -96,13 +96,6 @@ app.register(fastifyCookie, {
   parseOptions: {}, // options for parsing cookies
 });
 
-// Preflight Handling
-app.options('*', (request, reply) => {
-  reply.header('Access-Control-Allow-Origin', getAllowedOrigins());
-  reply.header('Access-Control-Allow-Credentials', 'true');
-  reply.header('Access-Control-Allow-Headers', 'Content-Type,x-api-key,Authorization');
-  reply.send();
-});
 
 // Register the CORS plugin after cookies
 app.register(fastifyCors, {
